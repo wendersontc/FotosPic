712678d4f23336d8f92bb3e4b78f6b0d05bf255b
Wenderson Roberto <wendersontc@gmail.com>
2017-05-30 01:23:41 -0300

    Aplicação angular2

---
+++ client/app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+import { Http } from '@angular/http';
+@Component({
+    moduleId : module.id,
+    selector: 'app',
+    templateUrl: './app.component.html'
+})
+export class AppComponent {
+     fotos : Object[] = [];
+     constructor (http : Http) {
+         http
+         .get('v1/fotos')
+         .map(res => res.json())
+         .subscribe( fotos =>{
+              this.fotos = fotos;
+         }, erro => console.log(erro));
+     }
+} 
+
---
+++ client/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+import  { FotoModule } from './foto/foto.module';
+import { HttpModule } from '@angular/http';
+import 'rxjs/add/Operator/map';
+
+@NgModule({
+  imports:      [ BrowserModule , FotoModule , HttpModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
